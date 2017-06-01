9a95f7aac43fbd943b6000e6bb92e97db7c4295e
borislavcho <bkirilov321@gmail.com>
2017-06-01 21:12:52 +0300

    dsa

---
+++ 01. Stacks-And-Queues/Lab/Matching-Brackets/MatchingBrackets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatchingBrackets
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                stack.Push(i);
+            }
+
+            if (input[i] == ')')
+            {
+                var lastScopeIndexOpen = stack.Pop();
+                Console.WriteLine(input.Substring(lastScopeIndexOpen, i - lastScopeIndexOpen + 1));
+            }
+        }
+    }
+}
---
+++ 04. Sets-And-Dictionaries-Exercises/Exercises/Fix-Emails/FixEmails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FixEmails
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var dictionary = new Dictionary<string, string>();
+
+        while ((input = Console.ReadLine()) != "stop")
+        {
+            var email = Console.ReadLine();
+
+            if (dictionary.ContainsKey(input))
+            {
+                dictionary[input] = email;
+            }
+            else
+            {
+                dictionary.Add(input, email);
+            }
+        }
+
+        foreach (var name in dictionary.Keys)
+        {
+            var email = dictionary[name];
+            var domain = email.Substring(email.Length - 2);
+
+            if (domain.ToLower() == "us" || domain.ToLower() == "uk")
+            {
+                dictionary.Remove(name);
+                continue;
+            }
+
+            Console.WriteLine($"{name} -> {dictionary[name]}");
+        }
+    }
+}
