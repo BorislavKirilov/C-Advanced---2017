f9faf0795f7ccb9eec74f2d71122e80412c86bbb
BorislavKirilov <bobi4cveti@gmail.com>
2017-06-04 14:43:53 +0300

    2 NEW EX

---
+++ 07. Streams/Lab/Writing-To-File/WritingToFile.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+class WritingToFile
+{
+    static void Main()
+    {
+        string text = "Кирилица";
+        var fileStream = new FileStream("../../log.txt", FileMode.Create);
+
+        try
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            fileStream.Write(bytes, 0, bytes.Length);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+    }
+}
